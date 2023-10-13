5ce17c97a18925e62c7794af960c0b108e01bec1
muli <muli@muli.dev>
2023-10-13 09:14:26 +0300

    project description

---
+++ README.md
@@ -0,0 +1,29 @@
+# Log-Q
+
+This script is designed to help you manage different pythons application by handling the Python installation, virtual environment setup, and running the application. Here's how the script works:
+
+## Prerequisites
+
+- Ensure you have Python 3 installed on your system.
+
+## Usage
+
+1. **Clone the Repository:**
+
+   ```bash
+   git clone https://github.com/go-isele/log-Q.git
+   cd log-Q
+
+
+This Bash script is designed to automate the process of running a FastAPI application. It includes the following functionalities:
+
+1. Checking if Python 3 is installed.
+2. Creating and activating a virtual environment (if Python is not installed).
+3. Installing requirements from `requirements.txt` (optional).
+4. Running the python application api using `uvicorn`.
+
+## Usage
+To run this script:
+```bash
+./run.sh
+
--- scripts/cleaner.py
+++ scripts/cleaner.py
@@ -1,13 +1,15 @@
 import os
 import datetime
 import time
-
+import logging
 import yaml
+
+# Configure logging to display messages on the console (stdout)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load the YAML configuration file
 with open('../config.yml') as f:
     config = yaml.safe_load(f)
-
 
 def remove_old_and_big_files(directory, max_age_days, max_size_bytes):
     # Get the current date
@@ -27,9 +29,16 @@ def remove_old_and_big_files(directory, max_age_days, max_size_bytes):
 
             # Check if the file is both old and exceeds the specified size
             if age_days > max_age_days and os.path.getsize(file_path) > max_size_bytes:
-                # Delete the file
-                os.remove(file_path)
+                try:
+                    # Delete the file
+                    os.remove(file_path)
+                    logging.info(f'Removed file: {file_path}')
+                except Exception as e:
+                    logging.error(f'Error occurred while removing file {file_path}: {str(e)}')
+                    print(f'Error occurred: {str(e)}')  # Print error for debugging
+                    raise  # Raise the exception for further handling or debugging
 
+    print('remove_old_and_big_files function execution completed')  # Debugging print statement
 
 # Example usage:
 if __name__ == "__main__":
@@ -38,6 +47,16 @@ if __name__ == "__main__":
     max_size = config['max_size_bytes']  # Maximum size in bytes
 
     while True:
-        remove_old_and_big_files(directory_path, max_age, max_size)
-        # Sleep for 1 minute (60 seconds) before the next check
-        time.sleep(60)
+        try:
+            remove_old_and_big_files(directory_path, max_age, max_size)
+            # Sleep for 5 seconds before the next check
+            time.sleep(5)
+        except KeyboardInterrupt:
+            logging.info('Script interrupted by user')
+            break
+        except Exception as e:
+            logging.error(f'An error occurred: {str(e)}')
+            print(f'An error occurred: {str(e)}')  # Print error for debugging
+            raise  # Raise the exception for further handling or debugging
+
+    print('Script execution completed')  # Debugging print statement
